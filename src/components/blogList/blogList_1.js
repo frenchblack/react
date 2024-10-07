12e3573c783006301203f328ae10abe07db98ab4
frenchblack <qkfqkfdl387@naver.com>
2024-10-07 23:18:12 +0900

    asdd

--- src/components/blogList/blogList_1.js
+++ src/components/blogList/blogList_1.js
@@ -2,6 +2,7 @@ import styles from  "./blogList_1.module.css";
 import { useState, useEffect, useRef } from "react"
 
 function BlogList_1() {
+    //aa
     const [list, setList] = useState([]);
     const [detail, setDetail] = useState({});
     const testList = () => {
