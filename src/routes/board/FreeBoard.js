b0d1a9afbd03bc0241db6a91af33271f39822d3b
frenchblack <qkfqkfdl387@naver.com>
2025-05-13 21:18:52 +0900

    * 작업 내용  - 자유게시판 조회 개발 중      - 리스트 조회 개발      - 백엔드 board 개발      - board 테이블 생성

--- src/routes/board/FreeBoard.js
+++ src/routes/board/FreeBoard.js
@@ -1,17 +1,33 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { useLocation } from "react-router-dom"
-import { MenuContext, getMenuName } from "util";
+import { MenuContext, getMenuName, nonAuthGet } from "util";
 import styles from "./FreeBoard.module.css";
 
 function FreeBoard() {
   //===========================================================================
   //1.변수 선언
   //===========================================================================
-  const [id, setId] = useState("");
+  const [boardList, setBoardList] = useState();
   const menuName = getMenuName(useContext(MenuContext).menuList, useLocation().pathname);
+
+
   //===========================================================================
   //2.내부 함수
   //===========================================================================
+  useEffect(() => {
+      getBoardList(); 
+  }, []);
+
+  //게시판 리스트 불러오기
+  const getBoardList = async () => {
+      try {
+          const menu = await nonAuthGet("/getBoardList");
+          setBoardList(menu.data);
+      } catch(e) {
+
+      }
+  }
+
   //===========================================================================
   //3.event 함수
   //===========================================================================  
@@ -20,7 +36,27 @@ function FreeBoard() {
   //=========================================================================== 
   return (
     <div className={`${ styles.Home } container`}>
-      {menuName}
+      <h1 className={ styles.menu_nm }>
+        {menuName}
+      </h1>
+      <div className={styles.list_div}>
+        <div className={styles.listHeader}>
+          <div className={styles.listItem1}>글번호</div>
+          <div className={styles.listItem2}>카테고리</div>
+          <div className={styles.listItem3}>제목</div>
+          <div className={styles.listItem4}>추천</div>
+          <div className={styles.listItem5}>조회</div>
+        </div>
+        {boardList?.map((board) =>(
+          <div key={board.board_no} className={styles.list}>
+            <div className={styles.listItem1} >{board.board_no}</div>
+            <div className={styles.listItem2} >{board.category_nm}</div>
+            <div className={styles.listItem3} >{board.title}</div>
+            <div className={styles.listItem4} >{board.like_cnt}</div>
+            <div className={styles.listItem5} >{board.view_cnt}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
